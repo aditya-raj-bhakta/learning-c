41273e70b333facc66d94fa190f6fa92438ff01f
Aditya-sys-cell <Adityarajbhakta@gmail.com>
2024-09-25 14:27:20 +0530

    something

---
+++ matrix-addition.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int r1,r2,c1,c2,x,y;
+    printf("enter the number of rows and column for matrix A:\t");
+    scanf("%d%d",&r1,&c1);
+    printf("enter the number of rows and column for matrix B:\t");
+    scanf("%d%d",&r2,&c2);
+    int A[r1][c1],B[r2][c2],product[r1][c2];
+    if(c1!=r2)
+      {
+        printf("product not possible!");
+      }
+    else
+    {  for (x=0;x<r1;x++)
+        { for (y=0;y<c1;y++)
+          { printf("enter element at (%d,%d) for matrix A\t",x,y);
+            scanf("%d",&A[x][y]);
+          }
+           }
+        for (x=0;x<r1;x++)
+        { for (y=0;y<c1;y++)
+          { printf("enter element at (%d,%d) for matrix B\t",x,y);
+            scanf("%d",&B[x][y]);
+          }
+           }
+        for (x=0;x<r1;x++)
+        {   product[x][y]=0;
+            for (y=0;y<c2;y++)
+        {
+            product[x][y]+=A[x][y]*B[y][x];
+            printf("the element at (%d,%d) for product %d \n",x,y,product[x][y]);
+        }}
+        
+    }
+
+
+
+
+
+
+
+
+}
