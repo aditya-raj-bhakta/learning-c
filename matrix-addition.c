85e1d47edd50c52edf3942959c5415b49c4239b1
Aditya-sys-cell <Adityarajbhakta@gmail.com>
2024-09-25 23:01:57 +0530

    adding matrix addition and substraction

--- matrix-addition.c
+++ matrix-addition.c
@@ -6,40 +6,36 @@ int main(){
     scanf("%d%d",&r1,&c1);
     printf("enter the number of rows and column for matrix B:\t");
     scanf("%d%d",&r2,&c2);
-    int A[r1][c1],B[r2][c2],product[r1][c2];
-    if(c1!=r2)
+    int A[r1][c1],B[r2][c2],sum[r1][c1];
+    if(r1!=r2||c1!=c2)
       {
-        printf("product not possible!");
+        printf("sum not possible!");
       }
     else
-    { for (x=0;x<r1;x++)
+    {  for (x=0;x<r1;x++)
         { for (y=0;y<c1;y++)
           { printf("enter element at (%d,%d) for matrix A\t",x,y);
             scanf("%d",&A[x][y]);
           }
            }
-      for (x=0;x<r1;x++)
+        for (x=0;x<r1;x++)
         { for (y=0;y<c1;y++)
           { printf("enter element at (%d,%d) for matrix B\t",x,y);
             scanf("%d",&B[x][y]);
           }
            }  
-      for (x=0;x<r1;x++)
+        for (x=0;x<r1;x++)
         { for (y=0;y<c2;y++)
-            {product[x][y]=0;
-              for (z=0;z<c1;z++)
-            {
-             product[x][y]+=A[x][z]*B[z][y];
-            }}}
-      for (x=0;x<r1;x++)
+          { sum[x][y]=0;
+            sum[x][y]=A[x][y]+B[x][y];
+
+           } 
+         }
+        for (x=0;x<r1;x++)
         { for (y=0;y<c2;y++)
-             { printf("the element at (%d,%d) for product %d \n",x,y,product[x][y]);}
-        }
-
-
+             { printf("the element at (%d,%d) for add %d \n",x,y,sum[x][y]);}
     }
 
-
-
-
-}
+    
+    }
+    }
--- matrix-substraction.c
+++ matrix-substraction.c
@@ -6,40 +6,36 @@ int main(){
     scanf("%d%d",&r1,&c1);
     printf("enter the number of rows and column for matrix B:\t");
     scanf("%d%d",&r2,&c2);
-    int A[r1][c1],B[r2][c2],product[r1][c2];
-    if(c1!=r2)
+    int A[r1][c1],B[r2][c2],diff[r1][c1];
+    if(r1!=r2||c1!=c2)
       {
-        printf("product not possible!");
+        printf("difference not possible!");
       }
     else
-    { for (x=0;x<r1;x++)
+    {  for (x=0;x<r1;x++)
         { for (y=0;y<c1;y++)
           { printf("enter element at (%d,%d) for matrix A\t",x,y);
             scanf("%d",&A[x][y]);
           }
            }
-      for (x=0;x<r1;x++)
+        for (x=0;x<r1;x++)
         { for (y=0;y<c1;y++)
           { printf("enter element at (%d,%d) for matrix B\t",x,y);
             scanf("%d",&B[x][y]);
           }
            }  
-      for (x=0;x<r1;x++)
+        for (x=0;x<r1;x++)
         { for (y=0;y<c2;y++)
-            {product[x][y]=0;
-              for (z=0;z<c1;z++)
-            {
-             product[x][y]+=A[x][z]*B[z][y];
-            }}}
-      for (x=0;x<r1;x++)
+          { diff[x][y]=0;
+            diff[x][y]=A[x][y]-B[x][y];
+
+           } 
+         }
+        for (x=0;x<r1;x++)
         { for (y=0;y<c2;y++)
-             { printf("the element at (%d,%d) for product %d \n",x,y,product[x][y]);}
-        }
-
-
+             { printf("the element at (%d,%d) for diffrence %d \n",x,y,diff[x][y]);}
     }
 
-
-
-
-}
+    
+    }
+    }
