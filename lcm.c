5e29e9f18129f4ad7be64c209a913fc8b1eb26e7
Aditya-sys-cell <Adityarajbhakta@gmail.com>
2024-09-30 15:15:23 +0530

    fixing lcm

--- lcm.c
+++ lcm.c
@@ -1,18 +1,13 @@
 #include<stdio.h>
-int main()
+int i;
+int lcm(int num1, int num2)
 {
-    int num1,num2,i;
-    printf("enter your 1st number:");
-    scanf("%d",&num1);
-    printf("enter the second number:");
-    scanf("%d",&num2);
     if (num1<num2)
      {
         for (i=num1;;i+=num1)
         {
             if (i%num2==0)
               {
-                printf("LCM is %d",i);
                 break;
               }
         }
@@ -23,9 +18,19 @@ int main()
         {
             if (i%num1==0)
               {
-                printf("LCM is %d",i);
                 break;
               }
         }
      }
+    return i;
+}
+int main()
+{
+    int num1,num2,c;
+    printf("enter your 1st number:");
+    scanf("%d",&num1);
+    printf("enter the second number:");
+    scanf("%d",&num2);
+    c=lcm(num1,num2);
+    printf("lcm is %d",c);
 }
